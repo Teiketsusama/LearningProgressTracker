44518c2adf1a967da3c72d4afbb12e4e7b69e43d
june <djie627@gmail.com>
2023-06-02 16:06:46 +1000

    Stage 4/5: Retrieving statistics New command: statistics, and the details about any course

--- task.py
+++ task.py
@@ -1,8 +1,10 @@
-import re
+from re import match
+from decimal import Decimal, ROUND_HALF_UP
 
 students = []
 list_of_ids = []
 students_points = {}
+submission = [0, 0, 0, 0]
 
 
 def add_students():
@@ -21,12 +23,12 @@ def add_students():
             email = input_list[-1]
             l_name = " ".join(input_list[1:-1])
 
-            if not re.match(r"^[a-zA-Z]+(?:['-](?=[a-zA-Z]))*[a-zA-Z]+$", f_name) or len(f_name) < 2:
+            if not match(r"^[a-zA-Z]+(?:['-](?=[a-zA-Z]))*[a-zA-Z]+$", f_name) or len(f_name) < 2:
                 print("Incorrect first name.")
-            elif not re.match(r"^[\sa-zA-Z]+(?:['-](?=[a-zA-Z]))*(?:[\sa-zA-Z]+(?:['-](?=[a-zA-Z]))*)*$", l_name) or \
+            elif not match(r"^[\sa-zA-Z]+(?:['-](?=[a-zA-Z]))*(?:[\sa-zA-Z]+(?:['-](?=[a-zA-Z]))*)*$", l_name) or \
                     len(l_name) < 2:
                 print("Incorrect last name.")
-            elif not re.match(r"^[a-zA-Z0-9._\-]+@[a-zA-Z0-9._\-]+\.[a-zA-Z0-9]+$", email):
+            elif not match(r"^[a-zA-Z0-9._\-]+@[a-zA-Z0-9._\-]+\.[a-zA-Z0-9]+$", email):
                 print("Incorrect email.")
             elif email in [student[2] for student in students]:
                 print("This email is already taken.")
@@ -61,9 +63,19 @@ def add_points():
                 print("Incorrect points format.")
             else:
                 if student_id not in students_points.keys():
-                    students_points[student_id] = [0, 0, 0, 0]
-                students_points[student_id] = [x + y for x, y in
-                                               zip(students_points[student_id], [c_py, c_dsa, c_db, c_fk])]
+                    students_points[student_id] = {"python": 0, "dsa": 0, "databases": 0, "flask": 0}
+                students_points[student_id]["python"] += c_py
+                if c_py > 0:
+                    submission[0] += 1
+                students_points[student_id]["dsa"] += c_dsa
+                if c_dsa > 0:
+                    submission[1] += 1
+                students_points[student_id]["databases"] += c_db
+                if c_db > 0:
+                    submission[2] += 1
+                students_points[student_id]["flask"] += c_fk
+                if c_fk > 0:
+                    submission[3] += 1
                 print("Points updated.")
         except ValueError:
             print("Incorrect points format.")
@@ -78,9 +90,106 @@ def show_points():
         if input_id not in list_of_ids:
             print(f"No student is found for id={input_id}.")
         else:
-            points_list = students_points[input_id]
+            points_list = [students_points[input_id]["python"], students_points[input_id]["dsa"],
+                           students_points[input_id]["databases"], students_points[input_id]["flask"]]
             print(f"{input_id} points: "
                   f"Python={points_list[0]}; DSA={points_list[1]}; Databases={points_list[2]}; Flask={points_list[3]}")
+
+
+def course_statistics(course: str) -> list:
+    n_enrolled = 0
+    sum_points = 0
+    avg_points = 0
+    # Calculate the number of students who have enrolled the course, and average points
+    for points in students_points.values():
+        if points[course] > 0:
+            n_enrolled += 1
+            sum_points += points[course]
+    if n_enrolled > 0:
+        avg_points = sum_points / n_enrolled
+    # Submission
+    if course == "python":
+        n_submission = submission[0]
+    elif course == "dsa":
+        n_submission = submission[1]
+    elif course == "databases":
+        n_submission = submission[2]
+    elif course == "flask":
+        n_submission = submission[3]
+
+    return [n_enrolled, n_submission, avg_points]
+
+
+def course_comparison() -> list:
+    courses = {
+        "Python": course_statistics("python"),
+        "DSA": course_statistics("dsa"),
+        "Databases": course_statistics("databases"),
+        "Flask": course_statistics("flask")
+    }
+    # Compare the number of students enrolled in each course
+    most_popular = []
+    least_popular = []
+    [most_popular.append(course) for course in courses
+     if 0 < courses[course][0] == max(courses.values(), key=lambda x: x[0])[0]]
+    [least_popular.append(course) for course in courses
+     if 0 < courses[course][0] == min(courses.values(), key=lambda x: x[0])[0] and course not in most_popular]
+    # Compare the number of completed tasks in each course
+    highest_activity = []
+    lowest_activity = []
+    [highest_activity.append(course) for course in courses
+     if 0 < courses[course][1] == max(courses.values(), key=lambda x: x[1])[1]]
+    [lowest_activity.append(course) for course in courses
+     if 0 < courses[course][1] == min(courses.values(), key=lambda x: x[1])[1] and course not in highest_activity]
+    # Compare the average points in each course
+    easiest_course = []
+    hardest_course = []
+    [easiest_course.append(course) for course in courses
+     if 0 < courses[course][2] == max(courses.values(), key=lambda x: x[2])[2]]
+    [hardest_course.append(course) for course in courses
+     if 0 < courses[course][2] == min(courses.values(), key=lambda x: x[2])[2] and course not in easiest_course]
+
+    return [most_popular, least_popular, highest_activity, lowest_activity, easiest_course, hardest_course]
+
+
+def display_statistics():
+    print("Type the name of a course to see details or 'back' to quit:")
+    comparison_list = ["Most popular", "Least popular", "Highest activity", "Lowest activity", "Easiest course",
+                       "Hardest course"]
+    for i in range(len(comparison_list)):
+        if len(course_comparison()[i]) == 1:
+            print(f"{comparison_list[i]}: {course_comparison()[i][0]}")
+        elif len(course_comparison()[i]) > 1:
+            print(f"{comparison_list[i]}: {', '.join(course_comparison()[i])}")
+        else:
+            print(f"{comparison_list[i]}: n/a")
+
+    while True:
+        input_course = input().strip().lower()
+        if input_course == "back":
+            break
+        elif input_course in ["python", "dsa", "databases", "flask"]:
+            """
+            To complete each of these courses, a student must earn a certain number of points that are different 
+            for each course: 600 for Python, 400 for DSA, 480 for Databases, and 550 for Flask.
+            The list must be sorted by the total number of points in descending order, and if two or more students
+            have the same number of points, they must be sorted by their ID in ascending order.
+            """
+            sorted_students = sorted(students_points.items(), key=lambda x: (-x[1][input_course], x[0]))
+            print(f"{input_course.capitalize()}\nid     points    completed")
+            for student, points in sorted_students:
+                if input_course == "python":
+                    cpl_perc = Decimal(points[input_course] / 6).quantize(Decimal(".0"), ROUND_HALF_UP)
+                elif input_course == "dsa":
+                    cpl_perc = Decimal(points[input_course] / 4).quantize(Decimal(".0"), ROUND_HALF_UP)
+                elif input_course == "databases":
+                    cpl_perc = Decimal(points[input_course] / 4.8).quantize(Decimal(".0"), ROUND_HALF_UP)
+                elif input_course == "flask":
+                    cpl_perc = Decimal(points[input_course] / 5.5).quantize(Decimal(".0"), ROUND_HALF_UP)
+
+                print(f"{student} {points[input_course]:<6} {cpl_perc:>6}%")
+        else:
+            print("Unknown course.")
 
 
 def main():
@@ -95,6 +204,8 @@ def main():
             add_points()
         elif user_input == "find":
             show_points()
+        elif user_input == "statistics":
+            display_statistics()
         elif user_input == "back":
             print("Enter 'exit' to exit the program.")
         elif user_input == "exit":
